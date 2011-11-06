0261576095f1e6b2b75e76209dcc70f0068eed19
mlamarque <lamarque.matthieu@gmail.com>
2011-11-06 12:44:53 +0100

    search method

--- lib/allocine/movie_list.rb
+++ lib/allocine/movie_list.rb
@@ -1,7 +1,21 @@
 module Allocine
 
   class MovieList
-  
+    def movies
+      @movies ||= parse_movies
+    end
+    
+    private
+    def parse_movies
+      id, originalTitle = nil
+      movies = []
+      document["movie"].each do |element|
+        id = element['code']
+        title = element['originalTitle']
+        movies << Allocine::Movie.new(id, originalTitle)
+      end
+      movies
+    end
   end
-  
+
 end
--- lib/allocine/search.rb
+++ lib/allocine/search.rb
@@ -1,6 +1,31 @@
 module Allocine
   
   class Search < MovieList
+    attr_reader :query
+
+    # Initialize a new Allocine search with the specified query
+    #
+    #   search = Allocine::Search.new("Superman")
+    #
+    def initialize(query)
+      @query = query
+    end
+    
+    # Returns an array of Allocine::Movie objects for easy search result yielded.
+    def movies
+      @movies ||= parse_movies
+    end
+
+    private
+    def document
+      @document ||= Allocine::Search.query(@query)
+    end
+    
+    def self.query(query)
+      url = "http://api.allocine.fr/rest/v3/search?partner=YW5kcm9pZC12M3M&filter=movie,tvseries&q=#{query}&format=json"
+      JSON.parse(Net::HTTP.get_response(URI.parse(URI.encode(url))).body)["feed"] rescue nil
+    end
+        
     
   end
 end
